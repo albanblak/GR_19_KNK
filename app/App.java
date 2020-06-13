97135addeacdd29a00d65b1decbb9ee496faf438
= <=>
2020-06-13 20:28:51 +0200

    a

--- app/App.java
+++ app/App.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
